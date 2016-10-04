db08f64446fa69c3fa01d7336f174059bc3cba07
Christopher Lidbury <christopher.lidbury10@imperial.ac.uk>
2016-10-04 22:30:41 +0100

    Implemented environment and added interface for opencl. Link with opencl_interface. Also touched up opencl.h.

---
+++ src/opencl/interface.h
@@ -0,0 +1,40 @@
+// Interface for interacting with parts of the OpenCL interface.
+
+#ifndef OPENCL_INTERFACE_H_
+#define OPENCL_INTERFACE_H_
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "opencl/opencl.h"
+
+namespace opencl {
+
+// Get the platform for the specified name, or die trying.
+// The name will match if the passed name is a substring of the platform name as
+// given by getPlatformInfo().
+// The first platform with a matching name will be selected, hence giving an
+// ambiguous name will give an implementation-defined result.
+cl::Platform GetPlatformOrDie(const std::string& platform_name);
+
+// Works in the same way as GetPlatformOrDie().
+// Will only search through devices available for the specified platform.
+cl::Device GetDeviceOrDie(const cl::Platform platform,
+                          const std::string& device_name);
+
+// Check OpenCL error code, and die if it is not CL_SUCCESS.
+#define CHECK_OPENCL(err, msg)                             \
+  {                                                        \
+    cl_int err_ = (err);                                   \
+    if (err_ != CL_SUCCESS) {                              \
+      std::cerr << "[" << __FILE__ << ":" << __LINE__      \
+                << "] OpenCL CHECK FAIL with error code "  \
+                << err_ << "(" << msg << ")" << std::endl; \
+      exit(EXIT_FAILURE);                                  \
+    }                                                      \
+  }
+
+}  // namespace opencl
+
+#endif  // OPENCL_INTERFACE_H_
--- src/opencl/opencl.h
+++ src/opencl/opencl.h
@@ -3,10 +3,11 @@
 #ifndef OPENCL_OPENCL_H_
 #define OPENCL_OPENCL_H_
 
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+
 #include "third_party/CL/cl_platform.h"
 #include "third_party/CL/cl.h"
 #include "third_party/CL/cl_ext.h"
-//#include "third_party/CL/cl_platform.h"
 #include "third_party/CL/cl2.hpp"
 #include "third_party/CL/opencl.h"
 
