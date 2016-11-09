795348dc2cc29327d9ef0007b7620f26e5d461d9
Hugues Evrard <hugues.evrard@hevrard.org>
2016-11-09 22:03:25 +0000

    Add configurations of non-persistent task, run for all configurations

--- src/run_test.py
+++ src/run_test.py
@@ -15,13 +15,12 @@ ITERS         = 1
 PRINT         = fake_placeholder
 DATA_PRINT    = fake_placeholder
 NAME_OF_CHIP  = ""
-NON_PRST_FREQ = ""
 
 PROGRAMS = {
     "pannotia_color",
-    "pannotia_mis",
-    "pannotia_bc",
-    "pannotia_sssp"
+    # "pannotia_mis",
+    # "pannotia_bc",
+    # "pannotia_sssp"
 }
 
 PROGRAM_DATA = {
@@ -29,9 +28,9 @@ PROGRAM_DATA = {
     "pannotia_color" : [ { "input" : os.path.join("inputs", "color", "ecology1.graph"),
                            "solution" : os.path.join("solutions", "color_ecology.txt"),
                            "stat" : "color_ecology" },
-                         { "input" : os.path.join("inputs", "color", "G3_circuit.graph"),
-                           "solution" : os.path.join("solutions", "color_G3_circuit.txt"),
-                           "stat" : "color_G3_circuit" }
+                         # { "input" : os.path.join("inputs", "color", "G3_circuit.graph"),
+                         #   "solution" : os.path.join("solutions", "color_G3_circuit.txt"),
+                         #   "stat" : "color_G3_circuit" }
     ],
 
     "pannotia_mis" : [ { "input" : os.path.join("inputs", "color", "ecology1.graph"),
@@ -54,13 +53,21 @@ PROGRAM_DATA = {
                           "stat" : "sssp_usa_road" } ]
 }
 
+# TODO: make it per machine ?
+MATMULT_CONFIG = [
+    # This is for Hugues laptop
+    { "freq" : "70", "matdim" : "160", "name" : "light" },
+    { "freq" : "40", "matdim" : "160", "name" : "medium" },
+    { "freq" : "40", "matdim" : "260", "name" : "high" },
+]
+
 def my_print(file_handle, data):
     print(data)
     file_handle.write(data + os.linesep)
 
 def exec_cmd(cmd, extr_args=[], flag=""):
     finalcmd = cmd + extr_args
-    PRINT("====================== RUN COMMAND =======================")
+    PRINT("====================== RUN START =========================")
     PRINT(flag)
     for s in finalcmd:
         PRINT(s)
@@ -75,7 +82,7 @@ def exec_cmd(cmd, extr_args=[], flag=""):
     PRINT("----------------------------------------------------------")
     str_ret_code = "Return code: " + str(ret_code)
     PRINT(str_ret_code)
-    PRINT("==========================================================")
+    PRINT("====================== RUN END ===========================")
     if (ret_code != 0):
         print("Error running " + " ".join(cmd))
         exit(ret_code)
@@ -102,19 +109,44 @@ def collect_stats(d):
 def run_suite():
     for p in PROGRAMS:
         for d in PROGRAM_DATA[p]:
-            exe = os.path.join(EXE_PATH, p)
-            graph_in = os.path.join(DATA_PATH, d["input"])
-            graph_sol = os.path.join(DATA_PATH, d["solution"])
-			# TODO: run merged skip task, grab occupancy result, and run standalone with as many wgs as the occupancy found
-            cmd = [exe, "--non_persistent_frequency", NON_PRST_FREQ, "--graph_file", graph_in, "--graph_solution_file", graph_sol, "--threads_per_wg", "128"]
-            # standalone, which does not produce stat files, so nothing to collect here
-            # exec_cmd(cmd, ["--run_persistent", "2"], "== standalone")
-            # merged without tasks, stats to collect afterwards
-            # exec_cmd(cmd, ["--skip_tasks", "1", "--merged_iterations", "2"], "== merged without task")
-            # collect_stats(d)
-            # merged
-            exec_cmd(cmd, ["--merged_iterations", "1"], "== merged")
-            collect_stats(d)
+            for c in MATMULT_CONFIG:
+                exe = os.path.join(EXE_PATH, p)
+                graph_in = os.path.join(DATA_PATH, d["input"])
+                graph_sol = os.path.join(DATA_PATH, d["solution"])
+                cmd = [exe]
+                cmd = cmd + ["--graph_file", graph_in]
+                cmd = cmd + ["--graph_solution_file", graph_sol]
+                cmd = cmd + ["--threads_per_wg", "128"]
+                cmd = cmd + ["--non_persistent_frequency", c["freq"]]
+                cmd = cmd + ["--matdim", c["matdim"]]
+                cmd = cmd + ["--output_summary", "summary_" + c["name"]]
+                cmd = cmd + ["--output_non_persistent_duration", "non_persistent_duration_" + c["name"]]
+                cmd = cmd + ["--output_timestamp_executing_groups", "timestamp_executing_groups_" + c["name"]]
+                cmd = cmd + ["--output_timestamp_non_persistent", "timestamp_non_persistent_" + c["name"]]
+                exec_cmd(cmd, [], c["name"])
+                collect_stats(d)
+
+                # in this order:
+                # 1. run merged skip task
+                # 2. grab occupancy
+                # 3. run standalone with same occupancy
+                # for the 3 configurations of non-persistent:
+                #   4. run merged for 3 configurations of non_persistent
+                #   5. run non_persistent for the nb of iterations there was in the merged run
+
+                # TODO: run merged skip task, grab occupancy result, and run standalone with as many wgs as the occupancy found
+
+                # standalone, which does not produce stat files, so nothing to collect here
+                # exec_cmd(cmd, ["--run_persistent", "2"], "== standalone")
+                # merged without tasks, stats to collect afterwards
+                # exec_cmd(cmd, ["--skip_tasks", "1", "--merged_iterations", "2"], "== merged without task")
+                # collect_stats(d)
+                # merged
+                #exec_cmd(cmd, ["--merged_iterations", "1"], "== merged")
+
+
+# There is a summary when we run skiptask, but there is no summary when
+# we run in standalone. Add one ?
 
 def main():
 
@@ -124,9 +156,9 @@ def main():
     global PRINT
     global DATA_PRINT
     global NAME_OF_CHIP
-    global NON_PRST_FREQ
+    global MATMULT_CONFIG
 
-    if len(sys.argv) != 7:
+    if len(sys.argv) != 6:
         print("Please provide the following arguments:")
         print("path to executables, path to data, path to result (where to store them), name of run, name of chip, frequency (ms) of non-persistent kernel")
         return 1
@@ -140,8 +172,6 @@ def main():
     print("recording all to " + log_file)
     log_file_handle = open(log_file, "w")
     PRINT = lambda x : my_print(log_file_handle,x)
-
-    NON_PRST_FREQ = sys.argv[6]
 
     PRINT("Name of chip:")
     PRINT(NAME_OF_CHIP)
