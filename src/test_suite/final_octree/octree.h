1f1fcd4232b9972374c4c68d94c1c581b025ea41
Hugues Evrard <hugues.evrard@hevrard.org>
2016-11-11 16:45:44 +0000

    Update defaults values

--- src/test_suite/final_octree/octree.h
+++ src/test_suite/final_octree/octree.h
@@ -1,7 +1,7 @@
-DEFINE_int32(numParticles, 5000000, "number of particles to treat");
+DEFINE_int32(numParticles, 3000000, "number of particles to treat");
 DEFINE_int32(maxChildren, 10, "maximum number of children");
 DEFINE_int32(threads, 128, "number of threads");
-DEFINE_int32(pool_size, 10000, "size of task pools");
+DEFINE_int32(pool_size, 20000, "size of task pools");
 
 // ---
 DEFINE_string(restoration_ctx_path, "test_suite/final_octree/", "Path to restoration context");
